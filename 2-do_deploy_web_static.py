8082a545111a83fa5d62338fd42b7a72c82a11d8
Ngum12 <d.ngum@alustudent.com>
2024-02-16 11:22:13 -0800

    deploy web

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
-from fabric.api import *
-import shlex
+"""
+Writing a Fabric script that distributes
+an archive to my web servers
+"""
 import os
-
-
+from fabric.api import put, run, env
 env.hosts = ['3.85.84.57', '54.196.167.246']
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """
+    do_deploy fxn
+    """
+    if archive_path is None or not os.path.exists(archive_path):
         return False
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
+        file = archive_path.split("/")[-1]
+        file_name = file.split(".")[0]
+        path = "/data/web_static/releases/"
 
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+
+        run("mkdir -p {}{}/".format(path, file_name))
+
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            file, path, file_name))
+
+        run("rm /tmp/{}".format(file))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_name))
+
+        run("rm -rf {}{}/web_static".format(path, file_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+
+        run("ln -s {}{}/ /data/web_static/current".
+            format(path, file_name))
         return True
-    except:
+    except BaseException:
         return False
